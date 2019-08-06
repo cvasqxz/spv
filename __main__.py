05ea29cae857ba09e796826588d49be4adcdc066
cvasqxz <cvxz@pm.me>
2019-08-06 18:21:53 -0400

    monoscpace Inconsolata

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from utils.log import log_print
 from threading import Thread
 from node import start_conn
 from random import randint
+from time import sleep
 
 
 def main(config, network='bitcoin'):
@@ -18,25 +19,37 @@ def main(config, network='bitcoin'):
 
     connected = False
 
-    while not connected:
-        try:
-            # SELECT RANDOM NODE
-            random_node = seeds[randint(0, len(seeds) - 1)]
-            hostport = random_node[4]
+    while True:
+        while not connected:
+            log_print('main', 'starting connection process')
+            try:
+                # SELECT RANDOM NODE
+                random_node = seeds[randint(0, len(seeds) - 1)]
+                hostport = random_node[4]
 
-            # CONNECT SOCKET
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.connect(hostport)
-            log_print('socket', 'connecting to %s:%s' % hostport)
+                # CONNECT SOCKET
+                sock = socket(AF_INET, SOCK_STREAM)
+                sock.connect(hostport)
+                log_print('socket', 'connecting to %s:%s' % hostport)
 
-            # START THREAD
-            t = Thread(target=start_conn, args=(MAGIC, hostport, sock, ))
-            t.start()
+                # START THREAD
+                t = Thread(target=start_conn, args=(MAGIC, hostport, sock, ))
+                t.start()
 
-            connected = True
+                log_print('main', 'connection successfully')
+                connected = True
 
-        except Exception as e:
-            log_print('error', e)
+            except Exception as e:
+                log_print('error', e)
+
+        thread_alive = t.is_alive()
+
+        if thread_alive:
+            log_print('main', 'P2P thread: %s' % thread_alive)
+        else:
+            connected = False
+
+        sleep(10)
 
 
 if __name__ == "__main__":
--- utils/log.py
+++ utils/log.py
@@ -7,6 +7,8 @@ def log_print(msg_type, msg):
         msg_color = '\033[93m'
     elif msg_type == 'recv':
         msg_color = '\033[92m'
+    elif msg_type == 'main':
+        msg_color = '\033[91m'
     else:
         msg_color = '\033[95m'
 
